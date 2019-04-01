e057fa8dd6975d18384499a36e1ca97ab9ec59be
Akansh-Rai <akansh.rai@gmail.com>
2019-04-01 14:23:33 +0530

    Did some code modification in liquibase property

--- src/main/java/com/epam/ProductReviewServices/domain/ProductReview.java
+++ src/main/java/com/epam/ProductReviewServices/domain/ProductReview.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 public class ProductReview {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
 	private Integer productId;
